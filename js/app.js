dcdc26e84e1ae5c273f93a27bf0a49f4e0a2d366
Emily Fischer <emily.m.fischer@outlook.com>
2019-11-16 11:36:10 -0600

    update data.js and index.html

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
-// Get a reference to the table body
-var tbody = d3.select("tbody");
+// Get a reference to the table
+var tbody = d3.select("#table-area");
 
 // from data.js
 var tableData = data;
 
 // Select the button
-var button = d3.select("#filter-btn");
+// var button = d3.select("#filter-btn");
 
-button.on("click", function () {
+// button.on("click", function () {
 
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+//   // Select the input element and get the raw HTML node
+//   var inputElement = d3.select("#datetime");
 
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+//   // Get the value property of the input element
+//   var inputValue = inputElement.property("value");
 
-  console.log(inputValue);
-  console.log(tableData);
+//   console.log(inputValue);
+//   console.log(tableData);
 
-  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+//   var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
   
-  // clear the existing output
-  tbody.html("");
-  inputElement.property("value","");
+//   // clear the existing output
+//   tbody.html("");
+//   inputElement.property("value","");
 
-  console.log(filteredData);
+//   console.log(filteredData);
   // Step 5: Use d3 to update each cell's text with
   // sighting report values (datetime, city, state, country, shape, durationMinutes, comments )
   filteredData.forEach(function (tableData) {
@@ -37,6 +37,6 @@ button.on("click", function () {
       var cell = row.append("td");
       cell.text(value);
     });
-  });
-});
+//   });
+// });
 
