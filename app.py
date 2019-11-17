8fe4e973cfaa7b4238dc64543f0e123c611f381e
setsumei1974 <david.kaatmann@gmail.com>
2019-11-17 13:43:31 -0600

    App3.js and App.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Import Modules and Dependencies
 import os
 
 import pandas as pd
@@ -13,7 +14,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-
 #################################################
 # Database Setup
 #################################################
@@ -21,20 +21,59 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://vqwpldlnxxqrvl:ba6bd238c236357aa816db445bceb5e604d7e491fbdecfd5bd8aace8885fb953@ec2-174-129-253-45.compute-1.amazonaws.com:5432/dbba91nli2plqi"
 db = SQLAlchemy(app)
 
-# reflect an existing database into a new model
+#Reflect Database into a New Model
 Base = automap_base()
-# reflect the tables
+#Reflect a Table
 Base.prepare(db.engine, reflect=True)
 
-# Save references to each table
-Cities_Metadata = Base.classes.cities_metadata
-Cities = Base.classes.cities
+#Save References to the Table
+Cities_Metadata = Base.classes.citystatistics
 
 
+#Create App Routes
 @app.route("/")
 def index():
     """Return the homepage."""
-    return render_template("index.html")
+    return render_template("index3.html")
+
+
+@app.route("/names")
+def names():
+
+    # Use Pandas to Perform the SQL Query
+    pdquery = db.session.query(Cities_Metadata).statement
+    df = pd.read_sql_query(pdquery, db.session.bind)
+
+    # Return a list of the column names (sample names)
+    return jsonify(list(df.columns)[2])
+
+
+@app.route("/metadata/<cities>")
+def cities_metadata(cities):
+
+    """Return the MetaData for a given sample."""
+    sel = [
+        Cities_Metadata.metropolitanpopulation,
+        Cities_Metadata.averageincome,
+        Cities_Metadata.studio,
+        Cities_Metadata.onebedroom,
+        Cities_Metadata.twobedroom,
+    ]
+
+    results = db.session.query(*sel).filter(Cities_Metadata.cities == cities).all()
+
+    # Create a Dictionary Entry for Each Row of Metadata Information
+    cities_metadata = {}
+    for result in results:
+        cities_metadata["Population"] = result[0]
+        cities_metadata["Average Income"] = result[1]
+        cities_metadata["Studio Apartments"] = result[2]
+        cities_metadata["1BR Apartments"] = result[3]
+        cities_metadata["2BR Apartments"] = result[4]
+
+    print(cities_metadata)
+    return jsonify(cities_metadata)
+
 
 if __name__ == "__main__":
     app.run()
---
+++ js/app3.js
@@ -0,0 +1,48 @@
+// @TODO: Complete the following function that builds the metadata panel
+function constructMetadata(cities) {
+
+    // Use `d3.json` to fetch the metadata for a sample
+    d3.json(`/metadata/${cities}`).then((data) => {
+
+        // Use d3 to select the panel with id of `#sample-metadata`
+        let selectManipulatePanel = d3.select("#sample-metadata");
+
+        // Use `.html("") to clear any existing metadata
+        selectManipulatePanel.html("");
+
+        // Use `Object.entries` to add each key and value pair to the panel
+        // Hint: Inside the loop, you will need to use d3 to append new tags for each key-value in the metadata
+        Object.entries(data).forEach(([key, value]) => {
+            selectManipulatePanel.append("h6").text(`${key}:${value}`);
+        });
+    });
+};
+
+function init() {
+
+    // Grab a Reference to the Dropdown Select Element
+    let dropdownSelect = d3.select("#selDataset");
+    
+    // Use the List of Sample Names to Populate the Select Options
+    d3.json("/names").then((sampleNames) => {
+        sampleNames.forEach((sample) => {
+            dropdownSelect
+                .append("option")
+                .text(sample)
+                .property("value", sample)
+        });
+    
+        // Use the First Sample from the List to Build Initial Plots
+        let firstSample = sampleNames[0];
+        constructMetadata(firstSample);
+    });
+};
+    
+function collectNewData(newSample) {
+
+    // Fetch New Data Each Time a New Sample is Selected
+    constructMetadata(newSample);
+};
+    
+// Initialize the Dashboard
+init();
