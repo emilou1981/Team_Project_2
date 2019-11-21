8551f033b0e9422cf0e12425135b5ab00f885d1a
Griffin Diemer <griffinfdiemer@gmail.com>
2019-11-20 20:10:07 -0600

    Added 2nd Table

--- static/static/js/app3.js
+++ static/static/js/app3.js
@@ -13,7 +13,26 @@ function constructMetadata(cities) {
         // Use `Object.entries` to add each key and value pair to the panel
         // Hint: Inside the loop, you will need to use d3 to append new tags for each key-value in the metadata
         Object.entries(data).forEach(([key, value]) => {
-            selectManipulatePanel.append("h6").text(`${key}:${value}`);
+            selectManipulatePanel.append("h6").text(`${key}: ${value}`);
+        });
+    });
+};
+
+function constructMetadata2(cities) {
+
+    // Use `d3.json` to fetch the metadata for a sample
+    d3.json(`/metadata/${cities}`).then((data) => {
+
+        // Use d3 to select the panel with id of `#sample-metadata`
+        let selectManipulatePanel = d3.select("#sample-metadata2");
+
+        // Use `.html("") to clear any existing metadata
+        selectManipulatePanel.html("");
+
+        // Use `Object.entries` to add each key and value pair to the panel
+        // Hint: Inside the loop, you will need to use d3 to append new tags for each key-value in the metadata
+        Object.entries(data).forEach(([key, value]) => {
+            selectManipulatePanel.append("h6").text(`${key}: ${value}`);
         });
     });
 };
@@ -38,6 +57,39 @@ function init() {
     });
 };
     
+
+function init2() {
+
+    // Grab a Reference to the Dropdown Select Element
+    let dropdownSelect2 = d3.select("#selDataset2");
+    
+    // Use the List of Sample Names to Populate the Select Options
+    d3.json("/names").then((sampleNames) => {
+        sampleNames.forEach((sample) => {
+            if (sample !== sampleNames[18]) {
+                dropdownSelect2
+                    .append("option")
+                    .text(sample)
+                    .property("value", sample)
+            }else
+                dropdownSelect2
+                    .append("option")
+                    .text(sample)
+                    .property("value", sample)
+                    .attr('selected', true)
+
+                
+        });
+        console.log(sampleNames[18])
+        // Use the First Sample from the List to Build Initial Plots
+        let secondSample = sampleNames[18];
+        constructMetadata2(secondSample);
+    });
+};
+
+
+
+
 function collectNewData(newSample) {
 
     // Fetch New Data Each Time a New Sample is Selected
@@ -46,3 +98,5 @@ function collectNewData(newSample) {
     
 // Initialize the Dashboard
 init();
+init2();
+
